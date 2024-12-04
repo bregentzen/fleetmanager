2f5e9a511c56ae343771ef70ee7d0e034f136346
daniel <daniel.bregen@hs-osnabrueck.de>
2024-12-04 14:18:19 +0100

    views erstellt

--- app/Models/Ship.php
+++ app/Models/Ship.php
@@ -4,5 +4,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ship extends Model
 {
-   
+    protected $fillable = [
+        'ship_name',
+        'captain',
+        'length',
+        'capacity_teu',
+        'year_built',
+    ];
 }
---
+++ database/migrations/2024_12_04_120412_add_fields_to_ships_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('ships', function (Blueprint $table) {
+            $table->string('ship_name')->nullable();
+            $table->string('captain')->nullable();
+            $table->float('length')->nullable();
+            $table->integer('capacity_teu')->nullable();
+            $table->integer('year_built')->nullable();
+        });
+
+        DB::table('ships')->update(['ship_name' => 'default', 'captain' => 'default', 'length' => 0, 'capacity_teu' => 0, 'year_built' => 2000]);
+
+        Schema::table('ships', function (Blueprint $table) {
+            $table->string('ship_name')->nullable(false)->change();
+            $table->string('captain')->nullable(false)->change();
+            $table->float('length')->nullable(false)->change();
+            $table->integer('capacity_teu')->nullable(false)->change();
+            $table->integer('year_built')->nullable(false)->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('ships', function (Blueprint $table) {
+            $table->dropColumn(['ship_name', 'captain', 'length', 'capacity_teu', 'year_built']);
+        });
+    }
+};
